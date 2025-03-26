0acac1bf15d091bd8e62d5f87cea45d42e2b8071
Hércules Santos <116934166+thellllima02@users.noreply.github.com>
2025-03-26 18:00:39 -0300

    Update script.js

--- Desafio6/script.js
+++ Desafio6/script.js
@@ -1,9 +1,25 @@
+let lista = [];
+
 function adicionar() {
   var numero = Number(document.getElementById("txtNum").value);
   var res = document.getElementById("res");
-  res.innerHTML = `Um teste ${numero}`;
 
-  if(numero <=0 || numero >100){
-    alert('Digite um número valido')
+  if (numero <= 0 || numero > 100) {
+    alert("Digite um número valido entre 1 a 100");
+    return;
   }
+  if (lista.includes(numero)) {
+    alert("Número ja adicionado anteriormente");
+    limpar();
+    return;
+  }
+  lista.push(numero);
+
+  res.innerHTML = lista.map((num) => `<li>${num}</li>`).join("");
+  limpar();
+}
+
+function limpar() {
+  document.getElementById("txtNum").value = "";
+  document.getElementById("txtNum").focus();
 }
