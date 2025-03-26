9d9b3712935cf7991fb37fbadfe948d89ef37db9
Hércules Santos <116934166+thellllima02@users.noreply.github.com>
2025-03-26 16:33:30 -0300

    .

--- aula15/script.js
+++ aula15/script.js
@@ -1,8 +1,31 @@
 let num = [5,8,2,9,3];
 //num[3] = 6;
 //num.push(88);
-num.sort();
-console.log(`Npsso vetotr é o ${num}`);
-console.log(num)
+//num.sort();
+
+
 
 console.log(num.length)
+
+//1ª forma de Mostrar 
+console.log(num)
+
+//2ª forma de Mostrar 
+console.log(`Nosso vetotr é o ${num}`);
+
+//3ª forma de Mostrar
+console.log(num[0])
+console.log(num[1])
+console.log(num[2])
+console.log(num[3])
+console.log(num[4])
+
+//4ª forma de Mostrar
+for(let pos = 0; pos< num.length; pos++){
+    console.log(`A posição ${pos} tem  o valor ${num[pos]}`)
+}
+
+//5ª forma de Mostrar 
+for(let pos in num){
+    console.log(`A posição ${pos} tem  o valor ${num[pos]}`)
+}
